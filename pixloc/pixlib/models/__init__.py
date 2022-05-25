641dcb6efeffb1e8aeb8db6b1c99171a322367ab
sublatelog <blueganz@gmail.com>
2022-05-25 15:31:06 +0900

    Update __init__.py

--- pixloc/pixlib/models/__init__.py
+++ pixloc/pixlib/models/__init__.py
@@ -3,6 +3,7 @@ from .base_model import BaseModel
 
 # get_model(conf.model.name)(conf.model).to(device)
 # name: two_view_refiner
-# conf.model：model関係のconf
+# conf.model:model関係のconf
+# __name__:Python のプログラムがどこから呼ばれて実行されているかを格納している変数
 def get_model(name):
     return get_class(name, __name__, BaseModel)

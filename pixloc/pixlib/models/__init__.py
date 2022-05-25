531631b9389721db2e06e3db72149b6db822d178
sublatelog <blueganz@gmail.com>
2022-05-25 15:11:35 +0900

    Update __init__.py

--- pixloc/pixlib/models/__init__.py
+++ pixloc/pixlib/models/__init__.py
@@ -1,6 +1,8 @@
 from ..utils.tools import get_class
 from .base_model import BaseModel
 
-
+# get_model(conf.model.name)(conf.model).to(device)
+# name: two_view_refiner
+# conf.model：model関係のconf
 def get_model(name):
     return get_class(name, __name__, BaseModel)
